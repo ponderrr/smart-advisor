6fa443caadb4ae9f93079921059e05d91fa1a95e
vladfiffie <vladimirfiffiejr@proton.me>
2026-03-13 18:13:13 -0500

    chore: readded lottie animations

--- src/components/animated-svgs/index.ts
+++
@@ -1,4 +0,0 @@
-export { PrivateProfileSVG } from "./private-profile";
-export { WarpSpeedSVG } from "./warp-speed";
-export { EasyTrustSVG } from "./easy-trust";
-export { ChooseBothSVG } from "./choose-both";
